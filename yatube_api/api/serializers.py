d1cb17f192905d4b83be957720f096dde591e971
Catherine <catrain2020@gmail.com>
2021-09-23 14:00:38 +0300

    First commit

--- yatube_api/api/serializers.py
+++ yatube_api/api/serializers.py
@@ -1,16 +1,36 @@
 from rest_framework import serializers
-from rest_framework.relations import SlugRelatedField
+from rest_framework.relations import SlugRelatedField, StringRelatedField
+from rest_framework.validators import UniqueTogetherValidator, ValidationError
 
-
-from posts.models import Comment, Post
+from posts.models import Comment, Post, Group, Follow, User
 
 
 class PostSerializer(serializers.ModelSerializer):
     author = SlugRelatedField(slug_field='username', read_only=True)
+    image = StringRelatedField(read_only=True)
 
     class Meta:
-        fields = '__all__'
         model = Post
+        fields = ('__all__')
+
+
+class UserSerializer(serializers.ModelSerializer):
+    posts = serializers.SlugRelatedField(
+        many=True,
+        read_only=True,
+        slug_field='post'
+    )
+
+    class Meta:
+        model = User
+        fields = ('__all__')
+
+
+class GroupSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Group
+        fields = ('__all__')
 
 
 class CommentSerializer(serializers.ModelSerializer):
@@ -19,5 +39,34 @@ class CommentSerializer(serializers.ModelSerializer):
     )
 
     class Meta:
-        fields = '__all__'
         model = Comment
+        fields = ('__all__')
+
+
+class FollowSerializer(serializers.ModelSerializer):
+    user = serializers.SlugRelatedField(
+        slug_field='username',
+        queryset=User.objects.all(),
+        default=serializers.CurrentUserDefault()
+    )
+
+    following = serializers.SlugRelatedField(
+        slug_field='username',
+        queryset=User.objects.all()
+    )
+
+    class Meta:
+        model = Follow
+        fields = ('__all__')
+
+        validators = [
+            UniqueTogetherValidator(
+                queryset=Follow.objects.all(),
+                fields=('user', 'following')
+            )
+        ]
+
+    def validate(self, data):
+        if data['user'] == data['following']:
+            raise ValidationError('Подписка на самого себя запрещена')
+        return data
--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -1,1 +1,106 @@
-# TODO:  Напишите свой вариант
+from rest_framework import viewsets, filters, mixins
+from django.shortcuts import get_object_or_404
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.pagination import LimitOffsetPagination
+
+from posts.models import Post, Group, User, Follow
+from .serializers import (FollowSerializer, PostSerializer, GroupSerializer,
+                          CommentSerializer, UserSerializer)
+from .permissions import IsAuthorOrReadOnly
+
+
+class CreateListRetrieveViewSet(mixins.CreateModelMixin,
+                                mixins.RetrieveModelMixin,
+                                mixins.ListModelMixin,
+                                viewsets.GenericViewSet):
+    pass
+
+
+class CreateListViewSet(mixins.CreateModelMixin,
+                        mixins.ListModelMixin,
+                        viewsets.GenericViewSet):
+    pass
+
+
+class UpdateDestroyViewSet(mixins.UpdateModelMixin,
+                           mixins.DestroyModelMixin,
+                           viewsets.GenericViewSet):
+    pass
+
+
+class PostViewSet(viewsets.ModelViewSet):
+
+    """ Предоставляет возможность работать с постами:
+    создавать, редактировать, удалять.
+    Координирует разрешения на доступ.
+    """
+
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+    permission_classes = (IsAuthorOrReadOnly,)
+    pagination_class = LimitOffsetPagination
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
+
+
+class UserViewSet(viewsets.ReadOnlyModelViewSet):
+
+    """ Дает возможность использовать базу User.
+    Работает только на чтение.
+    """
+
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+
+class GroupViewSet(viewsets.ReadOnlyModelViewSet):
+
+    """ Предоставляет возможность создавать группы.
+    Координирует разрешения на доступ.
+    """
+
+    queryset = Group.objects.all()
+    serializer_class = GroupSerializer
+    pagination_class = LimitOffsetPagination
+    permission_classes = (IsAuthorOrReadOnly,)
+
+
+class CommentViewSet(CreateListRetrieveViewSet, UpdateDestroyViewSet):
+
+    """ Предоставляет возможность работать с комментариями:
+    создавать, редактировать, удалять.
+    Координирует разрешения на доступ.
+    """
+
+    permission_classes = (IsAuthorOrReadOnly,)
+    serializer_class = CommentSerializer
+    pagination_class = LimitOffsetPagination
+
+    def get_queryset(self):
+        post = get_object_or_404(Post, id=self.kwargs['post_id'])
+        commets = post.comments.all()
+        return commets
+
+    def perform_create(self, serializer):
+        serializer.save(
+            author=self.request.user, post=get_object_or_404(
+                Post, id=self.kwargs['post_id']
+            )
+        )
+
+
+class FollowViewSet(CreateListViewSet):
+
+    """ Предоставляет возможность создавать подписки.
+    Дает возможность поиска по автору и подписчику.
+    """
+
+    serializer_class = FollowSerializer
+    filter_backends = (filters.SearchFilter,)
+    search_fields = ('=following__username', '=user__username')
+    pagination_class = LimitOffsetPagination
+
+    def get_queryset(self):
+        queryset = Follow.objects.filter(user=self.request.user)
+        return queryset
